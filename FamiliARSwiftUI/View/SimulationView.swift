4019bc53d22a3d081e2a1dd2b151c4dd93782ef8
Shiddiq <syuhadashiddiq97@gmail.com>
2021-11-02 09:19:40 +0700

    scene ui

--- FamiliARSwiftUI/View/SimulationView.swift
+++ FamiliARSwiftUI/View/SimulationView.swift
@@ -7,12 +7,107 @@
 
 import Foundation
 import SwiftUI
+import RealityKit
+import ARKit
+import FocusEntity
+
+struct RealityKitView: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARView {
+        let view = ARView()
+        
+        // Start AR session
+        let session = view.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        session.run(config)
+
+                // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        view.addSubview(coachingOverlay)
+
+                // Set debug options
+        #if DEBUG
+        view.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
+        
+        // Handle ARSession events via delegate
+        context.coordinator.view = view
+        session.delegate = context.coordinator
+        
+        // Handle taps
+        view.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: context.coordinator,
+                action: #selector(Coordinator.handleTap)
+            )
+        )
+        
+        return view
+    }
+    
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, ARSessionDelegate {
+        weak var view: ARView?
+        var focusEntity: FocusEntity?
+
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            guard let view = self.view else { return }
+            print("Anchor added to the scene: ", anchors)
+            self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
+        }
+        
+        @objc func handleTap() {
+            guard let view = self.view, let focusEntity = self.focusEntity else { return }
+
+            // Create a new anchor to add content to
+            let anchor = AnchorEntity()
+            view.scene.anchors.append(anchor)
+            
+            // Add a Box entity with a blue material
+            let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
+            let material = SimpleMaterial(color: .blue, isMetallic: true)
+            let diceEntity = ModelEntity(mesh: box, materials: [material])
+            diceEntity.position = focusEntity.position
+
+            anchor.addChild(diceEntity)
+        }
+    }
+
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+}
 
 struct SimulationView: View {
     var body: some View {
-        VStack{
-            Text("Simulation page")
-                .padding()
+        ZStack{
+            
+            RealityKitView()
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("Instruction")
+                    .padding()
+                ForEach((1...5), id: \.self) {
+                        Text("Step \($0)…")
+                            .padding()
+                            .background(Color(.red))
+                        Image(systemName: "checkmark.circle")
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            }
+            .background(Color(.gray))
+            .offset(x: 0, y: 0)
+            .frame(width: 200, height: 150, alignment: .leading)
         }
     }
 }
