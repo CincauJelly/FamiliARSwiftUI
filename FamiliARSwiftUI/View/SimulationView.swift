c6daaa55df094711b86ffa7dd58b67f839a04456
azrullahk <azrullahkainage@gmail.com>
2021-11-03 12:21:45 +0900

    Squashed commit of the following:
    commit 972338640e8c716573927590d7ceaad3492a73c4
    Merge: 092848f daad609
    Author: CincauJelly <82015920+CincauJelly@users.noreply.github.com>
    Date:   Wed Nov 3 10:04:07 2021 +0700
    
        Merge pull request #4 from CincauJelly/Maritia-sceen1
    
        Update schene
    
    commit 092848f9647d3e6b39be81ef037dce78393ffc9d
    Merge: b6d321d d73bade
    Author: CincauJelly <82015920+CincauJelly@users.noreply.github.com>
    Date:   Wed Nov 3 10:03:42 2021 +0700
    
        Merge pull request #3 from CincauJelly/zafappui
    
        UI
    
    commit daad60908bb2c08b612121fb76242ff28ac7e768
    Author: Maritia <maritia@qoala.id>
    Date:   Wed Nov 3 09:27:49 2021 +0700
    
        Update schene
    
    commit d73bade745af13a0087cf3ca37b1c6f84ab99abe
    Author: z <zafirafedhila@users.noreply.github.com>
    Date:   Tue Nov 2 21:46:31 2021 +0700
    
        UI
    
        app  icon, 2d asset. color pallete
    
    commit b6d321d4c97f576184a9e49bcb20de907594cd28
    Author: Shiddiq <syuhadashiddiq97@gmail.com>
    Date:   Tue Nov 2 10:57:29 2021 +0700
    
        UI Scene simulation
    
    commit b2a365543444b5708a8b0611c2bd7a50598f7b66
    Merge: 4019bc5 9db9f09
    Author: Shiddiq <syuhadashiddiq97@gmail.com>
    Date:   Tue Nov 2 09:19:49 2021 +0700
    
        Merge remote-tracking branch 'refs/remotes/origin/main'
    
    commit 4019bc53d22a3d081e2a1dd2b151c4dd93782ef8
    Author: Shiddiq <syuhadashiddiq97@gmail.com>
    Date:   Tue Nov 2 09:19:40 2021 +0700
    
        scene ui
    
    commit 9db9f095cac243e3681352b0330883ca6c3d07c2
    Merge: 8e69308 5862040
    Author: CincauJelly <82015920+CincauJelly@users.noreply.github.com>
    Date:   Tue Nov 2 09:17:15 2021 +0700
    
        Merge pull request #2 from CincauJelly/Maritia-sceen1
    
        Maritia Schene 1
    
    commit 5862040ef6c2b53e3404856da9be48e7b2cb9b9c
    Author: Maritia <maritia@qoala.id>
    Date:   Tue Nov 2 09:13:21 2021 +0700
    
        add scene 1
    
    commit 7341b3cbdfd0156e4618e63661daf6f7e79b962d
    Author: Maritia <maritia@qoala.id>
    Date:   Mon Nov 1 22:39:15 2021 +0700
    
        Update Assets, adjust scaling and behaviour user

--- FamiliARSwiftUI/View/SimulationView.swift
+++ FamiliARSwiftUI/View/SimulationView.swift
@@ -7,12 +7,130 @@
 
 import Foundation
 import SwiftUI
+import RealityKit
+import ARKit
+import FocusEntity
+
+struct RealityKitView: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARView {
+        let view = ARView()
+        
+        // Start AR session
+        let session = view.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        session.run(config)
+
+                // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        view.addSubview(coachingOverlay)
+
+                // Set debug options
+        #if DEBUG
+        view.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
+        
+        // Handle ARSession events via delegate
+        context.coordinator.view = view
+        session.delegate = context.coordinator
+        
+        // Handle taps
+        view.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: context.coordinator,
+                action: #selector(Coordinator.handleTap)
+            )
+        )
+        
+        return view
+    }
+    
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject, ARSessionDelegate {
+        weak var view: ARView?
+        var focusEntity: FocusEntity?
+
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            guard let view = self.view else { return }
+            print("Anchor added to the scene: ", anchors)
+            self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
+        }
+        
+        @objc func handleTap() {
+            guard let view = self.view, let focusEntity = self.focusEntity else { return }
+
+            // Create a new anchor to add content to
+            let anchor = AnchorEntity()
+            view.scene.anchors.append(anchor)
+            
+            // Add a Box entity with a blue material
+            let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
+            let material = SimpleMaterial(color: .blue, isMetallic: true)
+            let diceEntity = ModelEntity(mesh: box, materials: [material])
+            diceEntity.position = focusEntity.position
+
+            anchor.addChild(diceEntity)
+        }
+    }
+
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+}
 
 struct SimulationView: View {
     var body: some View {
-        VStack{
-            Text("Simulation page")
+        ZStack{
+            
+            RealityKitView()
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("Instruction")
+                    .padding()
+                    .foregroundColor(Color.red)
+                ForEach((1...5), id: \.self){i in
+                    HStack{
+                        Text("Step \(i)")
+                            .padding()
+                            .foregroundColor(Color.white)
+                            .frame(width: 300, alignment: .leading)
+                            .background(Color.red.opacity(0.6))
+                            .cornerRadius(15)
+                        Image(systemName: "checkmark.circle.fill")
+                            .padding()
+                            .foregroundColor(Color.white)
+                            .frame(width: 50)
+                    }
+                }
+            }
+            .padding()
+            .background(Color.gray.opacity(0.4))
+            .cornerRadius(15)
+            .position(x: -250, y: -120)
+            .frame(width: 250, height: 150, alignment: .leading)
+            
+            Text("00.00")
                 .padding()
+                .background(Color.white.opacity(0.0))
+                .cornerRadius(15)
+                .position(x: 600, y: -350)
+                .frame(width: 100, height: 50, alignment: .leading)
+            
+            Image(systemName: "speaker.fill")
+                .foregroundColor(Color.white)
+                .font(.system(size: 48))
+                .position(x: 0, y: 380)
+                .frame(width: 50, height: 50, alignment: .center)
         }
     }
 }
