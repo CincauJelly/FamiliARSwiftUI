0a3dbe466598dd709b48991c96b6ecb2de9ddf99
azrullahk <azrullahkainage@gmail.com>
2021-11-16 11:27:30 +0900

    Main page fix tap interaction

--- FamiliARSwiftUI/ContentView.swift
+++ FamiliARSwiftUI/ContentView.swift
@@ -10,13 +10,13 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-            SimulationView()
-        
+        MainView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
--- FamiliARSwiftUI/View/SimulationView.swift
+++ FamiliARSwiftUI/View/SimulationView.swift
@@ -12,31 +12,33 @@ import ARKit
 import FocusEntity
 
 struct RealityKitView: UIViewRepresentable {
+
     func makeUIView(context: Context) -> ARView {
         let view = ARView()
-        
-        // Start AR session
-        let session = view.session
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal]
-        session.run(config)
 
-                // Add coaching overlay
+        // Start AR session
+
+        let sceneState = makeCoordinator().sceneState
+
+        let session = view.session
+//        let config = ARWorldTrackingConfiguration()
+//        config.planeDetection = [.horizontal]
+//        session.run(config)
+
+        // Add coaching overlay
+
+
         let coachingOverlay = ARCoachingOverlayView()
         coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         coachingOverlay.session = session
         coachingOverlay.goal = .horizontalPlane
-        view.addSubview(coachingOverlay)
+        if sceneState == 0 {
+            view.addSubview(coachingOverlay)
+        }
 
-                // Set debug options
-        #if DEBUG
-        view.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
-        #endif
-        
-        // Handle ARSession events via delegate
         context.coordinator.view = view
         session.delegate = context.coordinator
-        
+
         // Handle taps
         view.addGestureRecognizer(
             UITapGestureRecognizer(
@@ -44,94 +46,132 @@ struct RealityKitView: UIViewRepresentable {
                 action: #selector(Coordinator.handleTap)
             )
         )
-        
+
+
         return view
     }
-    
-    
-    
+
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+            Coordinator()
     }
 
     class Coordinator: NSObject, ARSessionDelegate {
         weak var view: ARView?
+        var sceneState = 0
         var focusEntity: FocusEntity?
+        let skinBurnScene = try! Macro.loadStep1()
 
         func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            if self.sceneState == 0 {
+                guard let view = self.view else { return }
+//                print("Anchor added to the scene: ", anchors)
+                self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
+
+            }
+        }
+
+        @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
             guard let view = self.view else { return }
-            print("Anchor added to the scene: ", anchors)
-            self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
-        }
-        
-        @objc func handleTap() {
-            guard let view = self.view, let focusEntity = self.focusEntity else { return }
 
-            // Create a new anchor to add content to
-            let anchor = AnchorEntity()
-            view.scene.anchors.append(anchor)
-            
-            // Add a Box entity with a blue material
-            let box = MeshResource.generateBox(size: 0.5, cornerRadius: 0.05)
-            let material = SimpleMaterial(color: .blue, isMetallic: true)
-            let diceEntity = ModelEntity(mesh: box, materials: [material])
-            diceEntity.position = focusEntity.position
+            if sceneState == 0 {
+                self.sceneState += 1
 
-            anchor.addChild(diceEntity)
+                DispatchQueue.main.async {
+                    view.scene.addAnchor(self.skinBurnScene)
+                }
+            } else if self.sceneState == 1 {
+                self.sceneState += 1
+                self.skinBurnScene.notifications.charaAnimation.post()
+            } else {
+                if let tapLocation = sender?.location(in: view) {
+                    guard let rayResult = self.view?.ray(through: tapLocation) else {return}
+
+                    if let result = self.view?.scene.raycast(origin: rayResult.origin, direction: rayResult.direction) {
+                        for object in result {
+                            if object.entity.name == "stoveSwitch" {
+                                skinBurnScene.notifications.stoveSwitch.post()
+                            }
+//                            else if object.entity.name == "person" {
+//                                skinBurnScene.notifications.startStep2.post()
+//                            } else if object.entity.name == "kran" {
+//                                skinBurnScene.notifications.MovetoSink.post()
+//                            } else if object.entity.name == "cling" {
+//                                skinBurnScene.notifications.pickCling.post()
+//                            } else if object.entity.name == "person5" {
+//                                skinBurnScene.notifications.warpCling.post()
+//                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    
     func updateUIView(_ uiView: ARView, context: Context) {
-        
+
     }
+
+
 }
 
 struct SimulationView: View {
+    var currentStep = 0
+    var screenSize = UIScreen.main.bounds
+    
+    var steps: [String] = ["Stop the burning process",
+                           "Check skin surface splashed with hot water",
+                           "Cool the burn with cool running water",
+                           "Find cling film around",
+                           "Cover the burn with cling film"]
+    
     var body: some View {
         ZStack{
             
             RealityKitView()
                 .ignoresSafeArea()
             
-            VStack{
-                Text("Instruction")
-                    .padding()
-                    .foregroundColor(Color.red)
-                ForEach((1...5), id: \.self){i in
-                    HStack{
-                        Text("Step \(i)")
-                            .padding()
-                            .foregroundColor(Color.white)
-                            .frame(width: 300, alignment: .leading)
-                            .background(Color.red.opacity(0.6))
-                            .cornerRadius(15)
-                        Image(systemName: "checkmark.circle.fill")
-                            .padding()
-                            .foregroundColor(Color.white)
-                            .frame(width: 50)
-                    }
-                }
-            }
-            .padding()
-            .background(Color.gray.opacity(0.4))
-            .cornerRadius(15)
-            .position(x: -250, y: -120)
-            .frame(width: 250, height: 150, alignment: .leading)
-            
-            Text("00.00")
-                .padding()
-                .background(Color.white.opacity(0.0))
-                .cornerRadius(15)
-                .position(x: 600, y: -350)
-                .frame(width: 100, height: 50, alignment: .leading)
-            
-            Image(systemName: "speaker.fill")
-                .foregroundColor(Color.white)
-                .font(.system(size: 48))
-                .position(x: 0, y: 380)
-                .frame(width: 50, height: 50, alignment: .center)
+//            VStack{
+//                Text("Instruction")
+//                    .foregroundColor(Color("Color Primary"))
+//                ForEach((0...4), id: \.self){i in
+//                    HStack{
+//                        Text(steps[i])
+//                            .padding(8)
+//                            .foregroundColor(Color.white)
+//                            .frame(width: 300, alignment: .leading)
+//                            .background(i <= currentStep ? Color("Color Primary") : Color.white.opacity(0.6))
+//                            .cornerRadius(15)
+//                        Image(systemName: "checkmark.circle.fill")
+//                            .foregroundColor(i <= currentStep ? Color("Color Primary") : Color("Color Secondary 2"))
+//                            .frame(width: 50)
+//                    }
+//                    .padding(.leading, 12)
+//                    .padding(.top, 4)
+//                }
+//            }
+//            .background(Color.gray.opacity(0.8))
+//            .cornerRadius(15)
+//            .frame(width: screenSize.width * 0.95, height: screenSize.height * 0.95, alignment: .topLeading)
+//
+//            Text("00.00")
+//                .padding()
+//                .background(Color.white.opacity(0.0))
+//                .cornerRadius(15)
+//                .position(x: 600, y: -350)
+//                .frame(width: 100, height: 50, alignment: .leading)
+//
+//            Image(systemName: "speaker.fill")
+//                .foregroundColor(Color.white)
+//                .font(.system(size: 48))
+//                .position(x: 0, y: 380)
+//                .frame(width: 50, height: 50, alignment: .center)
         }
+    }
+}
+
+extension SimulationView {
+    func startSimulation () {
+        print("Hello")
     }
 }
 
