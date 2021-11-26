a14de8ddabf038bff2a7e961d1640db0dfe05c70
z <zafirafedhila@users.noreply.github.com>
2021-11-26 08:42:57 +0700

    onboarding
    ad onboarding and app storage

--- FamiliARSwiftUI/FamiliARSwiftUIApp.swift
+++ FamiliARSwiftUI/FamiliARSwiftUIApp.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 @main
 struct FamiliARSwiftUIApp: App {
+    @AppStorage ("Onboarding") var onboardingActive :Bool = true
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
+            if onboardingActive {
+                Onboarding()
+            }else{
+               SimulationView()
+            }
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
